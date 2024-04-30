6161b90958ca5418a3b62afb9324e3831ddc1348
Julien <julien@citio.digital>
2024-04-30 13:03:36 +0200

    feat: send hello ech 10 seconds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" }, },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
--- index.js
+++ index.js
@@ -69,11 +69,18 @@ function broadcastMessage(message) {
 }
 
 wss.on("connection", function connection(ws /*, req*/) {
-  //console.log('New connection');
-
   /*ws.on('open', function incoming(open) {
     //console.log('Open : %s', message);
   });*/
+
+  const interval_id = setInterval(function () {
+    ws.send(
+      JSON.stringify({
+        msg_type: "control:hello",
+        connection_timeout: 10000,
+      }),
+    );
+  }, 10000);
 
   ws.on("message", function incoming(message) {
     console.log("Data : %s", message);
@@ -81,16 +88,10 @@ wss.on("connection", function connection(ws /*, req*/) {
     if (js.msg_type == "data:subscribe_oauth") {
       tags[js.tag] = ws;
     }
-    ws.send(
-      JSON.stringify({
-        msg_type: "control:hello",
-        connection_timeout: 600000,
-      }),
-    );
   });
 
-  ws.on("close", function close() {
-    //console.log('Close');
+  ws.once("close", function close() {
+    clearInterval(interval_id);
     let keys = Object.keys(tags);
     for (let i = 0; i < keys.length; i++) {
       if (this == tags[keys[i]]) {
