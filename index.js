69444781fd22edce45bf65322d15e1387c2847a7
Julien <julien@citio.digital>
2026-05-15 01:05:16 +0200

    tmp: enable logging of messages in broadcastMessage function

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ function broadcastMessage(msg) {
         console.log(JSON.stringify(msg.raw));
         tags[msg.tag].send(JSON.stringify(msg.raw));
       } else {
-        //console.log(JSON.stringify(msg));
+        console.log(JSON.stringify(msg));
         tags[msg.tag].send(JSON.stringify(msg));
       }
     }
